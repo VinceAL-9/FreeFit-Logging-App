a69616715e35b1451d3f0108f47328a184cf6b99
Vince <vinceamiel.latabe-24@cpu.edu.ph>
2025-08-10 08:58:12 +0800

    Refactor font loading in App component and update app.json for font assets; add eas.json for EAS configuration

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 // App.tsx
+import * as Font from "expo-font";
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -10,6 +11,20 @@ import { WorkoutProvider } from './src/context/WorkoutContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
+  
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      "Roboto-Regular": require("./assets/fonts/roboto/Roboto-Regular.ttf"),
+      // ...more fonts
+      "strokeWeight-80-rotate-12": require("./assets/fonts/strokeweight/strokeWeight-80-rotate-12.otf"),
+      "strokeWeight-120-rotate-6": require("./assets/fonts/strokeweight/strokeWeight-120-rotate-6.otf"),
+    }).then(() => setFontsLoaded(true));
+  }, []);
+  if (!fontsLoaded) return null;
+  
+  
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
